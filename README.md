1f6062f88d2dc4143e9f01f5bf0d31456ad298a3
kagome <fu.taz.1102@gmail.com>
2024-07-01 13:20:42 +0900

    add option

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+## 参考
+
 - https://reffect.co.jp/react/react-router-6
 - https://kanterbury.hatenablog.com/entry/2022/12/20/125652
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -3,8 +3,12 @@ import { Link, Outlet, ScrollRestoration } from "react-router-dom"
 const Root = () => {
   return (
     <div>
-      {/* ページ遷移時にスクロール位置をリセットする */}
-      <ScrollRestoration />
+
+      <ScrollRestoration                /* ページ遷移時にスクロール位置をリセットする */
+        getKey={location => {           // ブラウザの戻るボタン以外でページ遷移した場合もスクロール位置を記憶するオプション
+          return location.pathname
+        }}
+      />
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
